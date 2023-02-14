e8db7858070794aa9a9a4ea1a46b13784b2516b5
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-02-13 22:26:50 -0600

    Completed the activity through a great amount of trial and error by editing the JavaScript in the script.js file repeatedly under guidance of the Northwestern Coding Bootcamp Staff

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,61 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Pools of Potential Password Characters as Strings
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz"
+
+var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var numericCharacters = "1234567890";
+
+var specialCharacters =  "!#$%&*+-.:;=?@^_|~";
+
+var passwordCharacterPool = "";
+
+// Generate password through Series of Prompts
+function generatePassword() {
+  // Prompt for Length of Password
+  var passwordLength = Number(prompt("Please enter a number between 8 and 128 to choose a length for your password to be."));
+  console.log("passwordLength", passwordLength)
+
+  // Prompts for Types of Characters to Include
+  var lowercaseLettersResponse = confirm("Would you like your password to contain lowercase letters?");
+  if (lowercaseLettersResponse) {
+    passwordCharacterPool += lowercaseLetters;
+  }
+
+  var uppercaseLettersResponse = confirm("Would you like your password to contain uppercase letters?");
+  if (uppercaseLettersResponse) {
+    passwordCharacterPool += uppercaseLetters;
+  }
+
+  var numericCharactersResponse = confirm("Would you like your password to contain numbers?");
+  if (numericCharactersResponse) {
+    passwordCharacterPool += numericCharacters;
+  }
+
+  var specialCharactersResponse = confirm("Would you like your password to contain special characters?");
+  if (specialCharactersResponse) {
+    passwordCharacterPool += specialCharacters;
+  }
+
+  console.log("passwordCharacterPool", passwordCharacterPool)
+
+  
+  // for Loop Using Responses to Generate Password  
+  var generatedPassword = []
+  
+  for(var i = 0; i < passwordLength; i++) {
+    var randomPoolIndex = Math.floor(Math.random() * (passwordCharacterPool.length - 0 + 1) + 0);
+    
+    generatedPassword += passwordCharacterPool[randomPoolIndex];
+    
+    console.log(generatedPassword)
+  }
+
+  return generatedPassword
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
